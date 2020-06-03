d3993f960a69e56a59a6fb9b7f0ac942a4b792ad
Michael Perez <mperez6473@gmail.com>
2020-06-03 15:29:34 -0500

    Have the server connected to the posts and users. Have the basic get link going at the moment.

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
-const express = require('express');
-
+const express = require("express");
+const morgan = require("morgan");
 const server = express();
 
-server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`);
-});
+const userRouter = require("./users/userRouter");
+const postsRouter = require("./posts/postRouter");
 
+server.get("/", (req, res) => {
+    res.send(`<h2>Let's write some middleware!</h2>`);
+});
+server.use(morgan("combined"));
+server.use(express.json());
+server.use("/api/users", userRouter);
+server.use("/api/posts", postsRouter);
 //custom middleware
 
-function logger(req, res, next) {}
+// function logger(req, res, next) {}
 
 module.exports = server;
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,47 +1,48 @@
-const express = require('express');
-
+const express = require("express");
+const Users = require("./userDb");
 const router = express.Router();
 
-router.post('/', (req, res) => {
-  // do your magic!
+router.post("/", (req, res) => {
+    // do your magic!
 });
 
-router.post('/:id/posts', (req, res) => {
-  // do your magic!
+router.post("/:id/posts", (req, res) => {
+    // do your magic!
 });
 
-router.get('/', (req, res) => {
-  // do your magic!
+router.get("/", (req, res) => {
+    // do your magic!
+    res.status(200).json(Users);
 });
 
-router.get('/:id', (req, res) => {
-  // do your magic!
+router.get("/:id", (req, res) => {
+    // do your magic!
 });
 
-router.get('/:id/posts', (req, res) => {
-  // do your magic!
+router.get("/:id/posts", (req, res) => {
+    // do your magic!
 });
 
-router.delete('/:id', (req, res) => {
-  // do your magic!
+router.delete("/:id", (req, res) => {
+    // do your magic!
 });
 
-router.put('/:id', (req, res) => {
-  // do your magic!
+router.put("/:id", (req, res) => {
+    // do your magic!
 });
 
 //custom middleware
 
 function validateUserId(req, res, next) {
-  // do your magic!
+    // do your magic!
 }
 
 function validateUser(req, res, next) {
-  // do your magic!
+    // do your magic!
 }
 
 function validatePost(req, res, next) {
-  // do your magic!
+    // do your magic!
 }
 
 module.exports = router;
