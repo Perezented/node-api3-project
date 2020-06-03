0c4963f1db8bad071399304f553498180496a019
Michael Perez <mperez6473@gmail.com>
2020-06-03 17:31:07 -0500

    restarted as I was having too many errors on starting up.

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
-const express = require('express');
+const express = require("express");
+
+const userRouter = require("./users/userRouter");
 
 const server = express();
 
-server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`);
+var morgan = require("morgan");
+
+server.use(morgan("combined"));
+server.use(express.json());
+server.use("/api/users", userRouter);
+server.get("/", (req, res) => {
+    res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
 //custom middleware
