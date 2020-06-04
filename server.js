f88719980f1a6efc7d1b9e8079e2bfda8a9adb9e
Michael Perez <mperez6473@gmail.com>
2020-06-04 16:52:01 -0500

    added the variable to heroku

--- server.js
+++ server.js
@@ -11,7 +11,11 @@ server.use(express.json());
 server.use("/api/users", userRouter);
 server.use("/api/posts", postsRouter);
 server.get("/", (req, res) => {
-    res.status(200).json({ env: process.env.NODE_ENV, port: process.env.PORT });
+    res.status(200).json({
+        env: process.env.NODE_ENV,
+        port: process.env.PORT,
+        stretch: process.env.STRETCH,
+    });
 });
 // server.get("/", (req, res) => {
 //     res.send(`<h2>Let's write some middleware!</h2>`);
